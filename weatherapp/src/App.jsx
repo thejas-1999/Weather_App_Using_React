84744f0a7d691aa8c50e3ec8e7f259466474736d
thejas-1999 <thejask074@gmail.com>
2024-01-22 16:37:10 +0530

    Apply the fundamentals styles

--- weatherapp/src/App.jsx
+++ weatherapp/src/App.jsx
@@ -2,8 +2,11 @@ import { useState } from "react";
 import axios from "axios";
 
 function App() {
-  /* const url =
-    "https://api.openweathermap.org/data/2.5/weather?q=tokyo&appid=c6211aa999dcaacbaec4e10e34f8efc4"; */
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState("");
+
+  const url =
+    "https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=c6211aa999dcaacbaec4e10e34f8efc4";
 
   return (
     <div className="app">
@@ -13,21 +16,25 @@ function App() {
             <p>Tokyo</p>
           </div>
           <div className="temp">
-            <h1>65F</h1>
+            <h1>65°F</h1>
           </div>
           <div className="description">
             <p>Clouds</p>
           </div>
         </div>
+
         <div className="bottom">
           <div className="feels">
-            <p>65F</p>
+            <p className="bold">65°F</p>
+            <p>Feels Like</p>
           </div>
           <div className="humidity">
-            <p>20%</p>
+            <p className="bold">20%</p>
+            <p>Humidity</p>
           </div>
           <div className="wind">
-            <p>12Mph</p>
+            <p className="bold">12MPH</p>
+            <p>wind Speed</p>
           </div>
         </div>
       </div>
