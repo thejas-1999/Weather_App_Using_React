d68c19cd0aaf9f6e3efcd53ab9e4f16ae6a4239e
thejas-1999 <thejask074@gmail.com>
2024-01-22 14:25:41 +0530

    Create an api key named as url

--- weatherapp/src/App.jsx
+++ weatherapp/src/App.jsx
@@ -1,5 +1,22 @@
+import { useState } from "react";
+import axios from "axios";
+
 function App() {
-  return <>Hello</>;
+  /* const url =
+    "https://api.openweathermap.org/data/2.5/weather?q=tokyo&appid=c6211aa999dcaacbaec4e10e34f8efc4"; */
+
+  return (
+    <div className="app">
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>Tokyo</p>
+          </div>
+        </div>
+        <div className="bottom"></div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
