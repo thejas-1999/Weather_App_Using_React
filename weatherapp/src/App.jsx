40e3bf57edde94cfdbe1eec50edb0a7f02288804
thejas-1999 <thejask074@gmail.com>
2024-01-22 19:22:57 +0530

    connect all the values in to api

--- weatherapp/src/App.jsx
+++ weatherapp/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   const [data, setData] = useState({});
   const [location, setLocation] = useState("");
 
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=c6211aa999dcaacbaec4e10e34f8efc4`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=c6211aa999dcaacbaec4e10e34f8efc4`;
 
   const searchLocation = (e) => {
     if (e.key === "Enter") {
@@ -31,30 +31,37 @@ function App() {
       <div className="container">
         <div className="top">
           <div className="location">
-            <p>Tokyo</p>
+            <p>{data.name}</p>
           </div>
           <div className="temp">
-            <h1>65°F</h1>
+            {data.main ? <h1>{data.main.temp.toFixed()}°C</h1> : null}
           </div>
           <div className="description">
-            <p>Clouds</p>
+            {data.weather ? <p>{data.weather[0].main}</p> : null}
           </div>
         </div>
+        {data.name !== undefined && (
+          <div className="bottom">
+            <div className="feels">
+              {data.main ? (
+                <p className="bold">{data.main.temp.toFixed()}°C</p>
+              ) : null}
 
-        <div className="bottom">
-          <div className="feels">
-            <p className="bold">65°F</p>
-            <p>Feels Like</p>
+              <p>Feels Like</p>
+            </div>
+            <div className="humidity">
+              {data.main ? <p className="bold">{data.main.humidity}%</p> : null}
+
+              <p>Humidity</p>
+            </div>
+            <div className="wind">
+              {data.wind ? (
+                <p className="bold">{data.wind.speed.toFixed()}MPH</p>
+              ) : null}
+              <p>Wind Speed</p>
+            </div>
           </div>
-          <div className="humidity">
-            <p className="bold">20%</p>
-            <p>Humidity</p>
-          </div>
-          <div className="wind">
-            <p className="bold">12MPH</p>
-            <p>wind Speed</p>
-          </div>
-        </div>
+        )}
       </div>
     </div>
   );
